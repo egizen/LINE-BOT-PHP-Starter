b720e46d592e7229753fa44e98ad7241e7354128
Saranyoo Suttikapluk <saranyoo.cmu@gmail.com>
2018-11-01 11:51:29 +0700

    Update callback.php

--- BotLine/callback.php
+++ BotLine/callback.php
@@ -30,8 +30,9 @@ foreach ($events as $event) {
 		
 		//fwd mobile inc from war room to team group.
 		if($event->isGroupEvent() && $event->getGroupId() == 'Ce70fd831ce381db12fcdfbdb1efa3875' && (stripos($text, 'k-plus') !== FALSE || stripos($text, 'k plus') !== FALSE || stripos($text, 'mobile') !== FALSE)) {
+			$groupIds = array('C5b971f1879eec72b49727f1498177f4c','Cb74bc2030e0cba40c51e9f77b41fd228');
 			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-			$response = $bot->pushMessage('C5b971f1879eec72b49727f1498177f4c', $textMessageBuilder);
+			$response = $bot->multicast($groupIds, $textMessageBuilder);
 		}
 		
  		if (strpos($text, '007 อยากรู้') !== FALSE) { //get info from wiki.
